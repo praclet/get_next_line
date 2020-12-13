181131320d30ed0782eed61b2c146de7c5e49203
Philippe Raclet <praclet@student.42lyon.fr>
2020-12-13 10:05:20 +0100

    Fix.

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,12 +6,14 @@
 /*   By: praclet <praclet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 22:21:11 by praclet           #+#    #+#             */
-/*   Updated: 2020/12/09 15:44:48 by praclet          ###   ########lyon.fr   */
+/*   Updated: 2020/12/13 10:01:25 by praclet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_BONUS_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
 
 typedef struct		s_file
 {
@@ -30,9 +32,9 @@ typedef struct		s_list
 }					t_list;
 
 int					get_next_line(int fd, char **line);
-char				*gnl_new_line(t_file *file, char *s1);
 int					ft_strlen(const char *str);
 t_file				*lst_get(t_list **list, int fd);
-char				*gnl_new_line(t_file *file, char *s1);
+char				*gnl_concat(char *s1, char *s2, size_t len2);
+void				lst_remove(t_list **list, t_file **file);
 
 #endif
